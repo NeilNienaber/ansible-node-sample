d081d8054edd25842820ae6b69d0c60c0e0704b5
Neil Nienaber <neil.nienaber@gmail.com>
2016-05-13 18:28:03 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ var config = require('./config');
 var app = require('http').createServer(function(req, res){
   jokeService.getRandomJoke(function(err, joke){
     res.writeHead(200);
-    res.end(joke);
+    res.end(joke + " edit");
   });
 });
 
